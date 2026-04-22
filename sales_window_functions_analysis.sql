a6af0357a19aa1beabd73239e620f671ca90c6f7
Sufi6692 <sayedsufikhalandar@gmail.com>
2026-04-22 23:34:24 +0530

    Add window function queries for sales and ranking analysis

--- sales_window_functions_analysis.sql
+++ sales_window_functions_analysis.sql
@@ -18,4 +18,91 @@ GROUP BY productid;
 Additionally provide details such order id & order date */
 
 SELECT 
-SUM(sales) over(order by order_id)
+	orderid,
+	orderdate,
+	productid,
+	SUM(sales) over( partition by productid) TotalSalesByProducts
+FROM orders;
+
+-- Find the total sales acorss all orders 
+-- additional provide details such order Id, order date
+
+
+SELECT 
+	orderid,
+    orderdate,
+	SUM(sales)Over() TotalSales
+FROM orders;
+
+-- Find the total sales for the each product
+-- Additionally provide details such order Id, order date
+
+SELECT 
+	orderid,
+	orderdate,
+	SUM(Sales) OVER(partition by productid) TotalSalesByProducts
+FROM orders;
+
+
+
+-- Find the total sales across all orders
+-- Find the total sales for each product 
+-- Additionally provide details such order Id, order date
+
+SELECT 
+	orderid,
+	orderdate,
+	productid,
+	sales,
+	SUM(sales) OVER() TotalSales,
+    SUM(sales) OVER(partition by productid) TotalSalesByProducts
+FROM orders;
+    
+    
+SELECT 
+	orderid,
+    orderdate,
+    productid,
+    sales,
+    orderstatus,
+    SUM(sales)OVER() TotalSales,
+    SUM(sales) OVER(partition by productid) TotalSalesByProducts,
+    SUM(sales) OVER(partition by productid,orderstatus) SalesByProductsAndStatus
+FROM orders;
+    
+    
+-- Rank each orders based on their sales from highest to lowest
+-- Additionally provide details such order Id, Order date
+
+
+SELECT 
+	orderid,
+    orderdate,
+    sales,
+    RANK()OVER(order by sales ASC) RankSalesf
+FROM orders;
+
+-- Frame 
+
+SELECT 
+	orderid,
+    orderdate,
+    orderstatus,
+    sales,
+	SUM(sales) OVER (partition by orderstatus order by orderdate 
+		ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) TotalSales
+FROM orders;
+
+
+
+-- Rank Customers based on their total sales
+SELECT 
+	customerid,
+    SUM(sales)TotalSales,
+    RANK()OVER(order by SUM(sales) DESC) RankCustomers
+FROM orders
+GROUP BY customerid;
+    
+
+
+
